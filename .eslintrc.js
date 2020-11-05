934e0bca4133052f01cc48dfab620147106d9d27
h-jennings <jenningsdhunter@gmail.com>
2020-11-05 18:01:16 -0500

    adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['warn', 'single'],
+    'linebreak-style': [1, 'unix'],
+    'no-unused-vars': 'warn',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 1,
+    'react/prop-types': 0,
+  },
+};
