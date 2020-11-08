d93275b4b467268c131cfd883c91550646582e85
Hunter Jennings <jenningsdhunter@gmail.com>
2020-11-08 10:37:27 -0500

    Adding new packages and next.config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = withPlugins([withBundleAnalyzer({})], nextConfig);
