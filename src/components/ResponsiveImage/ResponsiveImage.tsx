07fe905062c26f91ccbad934bd0a00dcb29e8412
h-jennings <jenningsdhunter@gmail.com>
2020-11-11 10:26:33 -0500

    Adding bg color to RI component

--- src/components/ResponsiveImage/ResponsiveImage.tsx
+++ src/components/ResponsiveImage/ResponsiveImage.tsx
@@ -1,21 +1,27 @@
 import Image from 'next/image';
 import styles from './ResponsiveImage.module.scss';
+import { Colors } from '@/models/colors';
 
 type ResponsiveImageProps = {
   height: number;
   width: number;
   src: string;
+  bgColor?: string;
 };
 
 export const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
   src,
   width,
   height,
+  bgColor = Colors.grey,
 }) => {
   return (
     <div
       className={styles.outer}
-      style={{ paddingTop: `${(height / width) * 100}%` }}
+      style={{
+        paddingTop: `${(height / width) * 100}%`,
+        backgroundColor: bgColor,
+      }}
       data-testid='responsive-image-container'>
       <div className={styles.inner}>
         <Image src={src} width={width} height={height} />
---
+++ src/models/colors.ts
@@ -0,0 +1,7 @@
+export enum Colors {
+  black = '#222222',
+  white = '#fcfcfc',
+  grey = '#a0abbb',
+  blue = '#3943b7',
+  orange = '#c16200',
+}
