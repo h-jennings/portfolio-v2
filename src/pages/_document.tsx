2e35ecf0dbc2901a3d4c4610b516bda018f8c9b8
Hunter Jennings <jenningsdhunter@gmail.com>
2020-11-19 20:36:05 -0500

    adding document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
