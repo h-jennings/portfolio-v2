8d8ceb71764a8c33fd1f9ecd266c9cd84e0f8513
Hunter Jennings <jenningsdhunter@gmail.com>
2020-12-26 10:27:58 -0500

    adding reveal animations to caffeinator page

--- src/pages/caffeinator.tsx
+++ src/pages/caffeinator.tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import React from 'react';
+import Media from 'react-media';
 
 import { Button } from '@/components/Button/Button';
 import { FullscreenLink } from '@/components/FullscreenLink/FullscreenLink';
@@ -14,6 +15,11 @@ import {
 } from '@/components/layouts/SplitLayout/SplitLayout';
 import { ProjectNavigationLinks } from '@/components/ProjectNavigationLinks/ProjectNavigationLinks';
 import { ResponsiveImage } from '@/components/ResponsiveImage/ResponsiveImage';
+import {
+  RevealContainerInView,
+  RevealContainerOnEnter,
+  RevealTextOverflowOnEnter,
+} from '@/components/Reveal/Reveal';
 import { RevealBox } from '@/components/RevealBox/RevealBox';
 import { Projects, projects } from '@/data/projects';
 import { decodeHtml } from '@/helpers/decode-html';
@@ -23,6 +29,7 @@ import {
   getPreviousProject,
 } from '@/helpers/get-projects';
 import { useScrollToTop } from '@/helpers/use-scroll-to-top';
+import { Breakpoints } from '@/models/breakpoints';
 
 interface CaffeinatorProps {
   projects: Projects;
@@ -64,101 +71,184 @@ const Caffeinator: NextPage<CaffeinatorProps> = ({ projects, pathname }) => {
       <NextSeo {...SEO} />
       <SplitLayout>
         <SplitContentLeft>
-          <h1 className='m-b-xl md:m-b-md'>{projectName}</h1>
-          <h2 className='m-b-lg lh-default'>{description}</h2>
+          <h1 className='m-b-xl md:m-b-md'>
+            <RevealTextOverflowOnEnter>{projectName}</RevealTextOverflowOnEnter>
+          </h1>
+          <RevealContainerOnEnter>
+            <h2 className='m-b-lg lh-default'>{description}</h2>
+          </RevealContainerOnEnter>
 
           <div
             className={classnames('d-flex space-x-lg m-b-xl', styles.details)}>
-            <ProjectNavigationLinks next={nextProject} previous={prevProject} />
+            <RevealContainerOnEnter>
+              <ProjectNavigationLinks
+                next={nextProject}
+                previous={prevProject}
+              />
+            </RevealContainerOnEnter>
 
             {/* make component */}
             <div style={{ flex: 1 }}>
               <ul className='space-y-md'>
                 <li>
-                  <h3 className='m-b-sm'>What I Did</h3>
-                  <p>Design and Development</p>
+                  <RevealContainerOnEnter>
+                    <h3 className='m-b-sm'>What I Did</h3>
+                    <p>Design and Development</p>
+                  </RevealContainerOnEnter>
                 </li>
                 <li>
-                  <h3 className='m-b-sm'>Tech</h3>
-                  <p>React, TypeScript, XState, Sass, Framer Motion, Webpack</p>
+                  <RevealContainerOnEnter>
+                    <h3 className='m-b-sm'>Tech</h3>
+                    <p>
+                      React, TypeScript, XState, Sass, Framer Motion, Webpack
+                    </p>
+                  </RevealContainerOnEnter>
                 </li>
               </ul>
             </div>
           </div>
-          <Button
-            target='_blank'
-            rel='noopener noreferrer'
-            type='link'
-            href={currentProject?.externalLink ?? ''}>
-            visit website
-          </Button>
+          <RevealContainerOnEnter>
+            <Button
+              target='_blank'
+              rel='noopener noreferrer'
+              type='link'
+              href={currentProject?.externalLink ?? ''}>
+              visit website
+            </Button>
+          </RevealContainerOnEnter>
         </SplitContentLeft>
         <SplitContentRight>
           <div className={styles.mediaContainer}>
             <div className={styles.image1}>
-              <ResponsiveImage
-                height={816}
-                width={1450}
-                src='/images/caffeinator/caffeinator-image-1.png'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <Media
+                queries={{ mobile: Breakpoints.lg }}
+                defaultMatches={{ mobile: false }}>
+                {(matches) => {
+                  const content = (
+                    <ResponsiveImage
+                      height={816}
+                      width={1450}
+                      src='/images/caffeinator/caffeinator-image-1.png'
+                      altText='screenshot of caffeinator homepage'
+                      bgColor={imagePlaceholderBackgroundColor}
+                    />
+                  );
+                  return (
+                    <>
+                      {matches.mobile ? (
+                        <RevealContainerInView>{content}</RevealContainerInView>
+                      ) : (
+                        <RevealContainerOnEnter>
+                          {content}
+                        </RevealContainerOnEnter>
+                      )}
+                    </>
+                  );
+                }}
+              </Media>
             </div>
             <div className={styles.image2}>
-              <ResponsiveImage
-                height={646}
-                width={808}
-                src='/images/caffeinator/caffeinator-image-2.png'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <Media
+                queries={{ mobile: Breakpoints.lg }}
+                defaultMatches={{ mobile: false }}>
+                {(matches) => {
+                  const content = (
+                    <ResponsiveImage
+                      height={646}
+                      width={808}
+                      src='/images/caffeinator/caffeinator-image-2.png'
+                      altText='screenshot of caffeinator homepage'
+                      bgColor={imagePlaceholderBackgroundColor}
+                    />
+                  );
+                  return (
+                    <>
+                      {matches.mobile ? (
+                        <RevealContainerInView>{content}</RevealContainerInView>
+                      ) : (
+                        <RevealContainerOnEnter>
+                          {content}
+                        </RevealContainerOnEnter>
+                      )}
+                    </>
+                  );
+                }}
+              </Media>
             </div>
             <div className={styles.text1}>
-              <p>
-                My goal for this project was simple: I wanted to learn the
-                concept of hierarchal finite state machines. To accomplish this,
-                I chose to use the growing JavaScript library: XState. In order
-                to learn the ins-and-outs of the programming concept, I decided
-                to make an app that was complex enough to go beyond the basics,
-                but still be a reasonable side project.
-              </p>
+              <Media
+                queries={{ mobile: Breakpoints.lg }}
+                defaultMatches={{ mobile: false }}>
+                {(matches) => {
+                  const content = (
+                    <p>
+                      My goal for this project was simple: I wanted to learn the
+                      concept of hierarchal finite state machines. To accomplish
+                      this, I chose to use the growing JavaScript library:
+                      XState. In order to learn the ins-and-outs of the
+                      programming concept, I decided to make an app that was
+                      complex enough to go beyond the basics, but still be a
+                      reasonable side project.
+                    </p>
+                  );
+                  return (
+                    <>
+                      {matches.mobile ? (
+                        <RevealContainerInView>{content}</RevealContainerInView>
+                      ) : (
+                        <RevealContainerOnEnter>
+                          {content}
+                        </RevealContainerOnEnter>
+                      )}
+                    </>
+                  );
+                }}
+              </Media>
             </div>
             <div className={styles.text2}>
-              <p>
-                The bulk of the application is essentially a gigantic state
-                machine with complex transitions based on user interactions and
-                time. This project completely changed the way I think about
-                front end interactions and application state. Since completing
-                this app, I&apos;ve taken these concepts and applied them to
-                production-level applications due to their framework-agnostic
-                nature.
-              </p>
+              <RevealContainerInView>
+                <p>
+                  The bulk of the application is essentially a gigantic state
+                  machine with complex transitions based on user interactions
+                  and time. This project completely changed the way I think
+                  about front end interactions and application state. Since
+                  completing this app, I&apos;ve taken these concepts and
+                  applied them to production-level applications due to their
+                  framework-agnostic nature.
+                </p>
+              </RevealContainerInView>
             </div>
 
             <div className={styles.image3}>
-              <ResponsiveImage
-                height={1160}
-                width={1450}
-                src='/images/caffeinator/caffeinator-image-3.png'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <RevealContainerInView>
+                <ResponsiveImage
+                  height={1160}
+                  width={1450}
+                  src='/images/caffeinator/caffeinator-image-3.png'
+                  altText='screenshot of caffeinator homepage'
+                  bgColor={imagePlaceholderBackgroundColor}
+                />
+              </RevealContainerInView>
             </div>
             <div className={classnames('fz-base', styles.text3)}>
-              <p className='fz-base'>
-                The original concept of Hierarchal Finite State Machines
-                (Statecharts) was formalized in the 1980s by computer scientist,
-                David Harel.
-              </p>
+              <RevealContainerInView>
+                <p className='fz-base'>
+                  The original concept of Hierarchal Finite State Machines
+                  (Statecharts) was formalized in the 1980s by computer
+                  scientist, David Harel.
+                </p>
+              </RevealContainerInView>
             </div>
             <div className={styles.image4}>
-              <ResponsiveImage
-                height={696}
-                width={1236}
-                src='/images/caffeinator/caffeinator-image-4.png'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <RevealContainerInView>
+                <ResponsiveImage
+                  height={696}
+                  width={1236}
+                  src='/images/caffeinator/caffeinator-image-4.png'
+                  altText='screenshot of caffeinator homepage'
+                  bgColor={imagePlaceholderBackgroundColor}
+                />
+              </RevealContainerInView>
             </div>
           </div>
         </SplitContentRight>
