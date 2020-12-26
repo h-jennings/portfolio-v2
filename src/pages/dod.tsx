d8696e98d699e337cacaf79f95a04f499713d65a
Hunter Jennings <jenningsdhunter@gmail.com>
2020-12-26 10:27:39 -0500

    adding reveal animations to dod page

--- src/pages/dod.tsx
+++ src/pages/dod.tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import React from 'react';
+import Media from 'react-media';
 
 import { FullscreenLink } from '@/components/FullscreenLink/FullscreenLink';
 import { MainLayout } from '@/components/layouts/MainLayout/MainLayout';
@@ -13,6 +14,11 @@ import {
 } from '@/components/layouts/SplitLayout/SplitLayout';
 import { ProjectNavigationLinks } from '@/components/ProjectNavigationLinks/ProjectNavigationLinks';
 import { ResponsiveImage } from '@/components/ResponsiveImage/ResponsiveImage';
+import {
+  RevealContainerInView,
+  RevealContainerOnEnter,
+  RevealTextOverflowOnEnter,
+} from '@/components/Reveal/Reveal';
 import { RevealBox } from '@/components/RevealBox/RevealBox';
 import { Projects, projects } from '@/data/projects';
 import { decodeHtml } from '@/helpers/decode-html';
@@ -22,6 +28,7 @@ import {
   getPreviousProject,
 } from '@/helpers/get-projects';
 import { useScrollToTop } from '@/helpers/use-scroll-to-top';
+import { Breakpoints } from '@/models/breakpoints';
 
 interface DoD {
   projects: Projects;
@@ -64,25 +71,40 @@ const DoD: NextPage<DoD> = ({ projects, pathname }) => {
       <NextSeo {...SEO} />
       <SplitLayout>
         <SplitContentLeft>
-          <h1 className='m-b-xl md:m-b-md'>{currentProject.name}</h1>
-          <h2 className='m-b-lg lh-default'>{description}</h2>
+          <h1 className='m-b-xl md:m-b-md'>
+            <RevealTextOverflowOnEnter>
+              {currentProject.name}
+            </RevealTextOverflowOnEnter>
+          </h1>
+          <RevealContainerOnEnter>
+            <h2 className='m-b-lg lh-default'>{description}</h2>
+          </RevealContainerOnEnter>
 
           <div className={classnames('d-flex space-x-lg', styles.details)}>
-            <ProjectNavigationLinks next={nextProject} previous={prevProject} />
+            <RevealContainerOnEnter>
+              <ProjectNavigationLinks
+                next={nextProject}
+                previous={prevProject}
+              />
+            </RevealContainerOnEnter>
 
             {/* make component */}
             <div style={{ flex: 1 }}>
               <ul className='space-y-md'>
                 <li>
-                  <h3 className='m-b-sm'>What I Did</h3>
-                  <p>Front End Development</p>
+                  <RevealContainerOnEnter>
+                    <h3 className='m-b-sm'>What I Did</h3>
+                    <p>Front End Development</p>
+                  </RevealContainerOnEnter>
                 </li>
                 <li>
-                  <h3 className='m-b-sm'>Tech</h3>
-                  <p>
-                    Angular, TypeScript, NgRx, Jest, Testing Library, Sass, D3,
-                    Azure AD
-                  </p>
+                  <RevealContainerOnEnter>
+                    <h3 className='m-b-sm'>Tech</h3>
+                    <p>
+                      Angular, TypeScript, NgRx, Jest, Testing Library, Sass,
+                      D3, Azure AD
+                    </p>
+                  </RevealContainerOnEnter>
                 </li>
               </ul>
             </div>
@@ -91,48 +113,76 @@ const DoD: NextPage<DoD> = ({ projects, pathname }) => {
         <SplitContentRight>
           <div className={styles.mediaContainer}>
             <div className={styles.image1}>
-              <ResponsiveImage
-                height={1160}
-                width={1450}
-                src='/images/f35/f35-devices-images.jpg'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <RevealContainerOnEnter>
+                <ResponsiveImage
+                  height={1160}
+                  width={1450}
+                  src='/images/f35/f35-devices-images.jpg'
+                  altText='screenshot of caffeinator homepage'
+                  bgColor={imagePlaceholderBackgroundColor}
+                />
+              </RevealContainerOnEnter>
             </div>
             <div className={styles.image2}>
-              <ResponsiveImage
-                height={646}
-                width={808}
-                src='/images/f35/f35-logo.jpg'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <RevealContainerInView threshold={0.4}>
+                <ResponsiveImage
+                  height={646}
+                  width={808}
+                  src='/images/f35/f35-logo.jpg'
+                  altText='screenshot of caffeinator homepage'
+                  bgColor={imagePlaceholderBackgroundColor}
+                />
+              </RevealContainerInView>
             </div>
             <div className={styles.text1}>
-              <p>
-                The F-35 Joint Strike Fighter Program needed a way to make sense
-                of data disseminated across a massive, multi-decade long project
-                to create the next generation of defense technology.
-              </p>
+              <Media
+                queries={{ mobile: Breakpoints.lg }}
+                defaultMatches={{ mobile: false }}>
+                {(matches) => {
+                  const content = (
+                    <p>
+                      The F-35 Joint Strike Fighter Program needed a way to make
+                      sense of data disseminated across a massive, multi-decade
+                      long project to create the next generation of defense
+                      technology.
+                    </p>
+                  );
+                  return (
+                    <>
+                      {matches.mobile ? (
+                        <RevealContainerInView>{content}</RevealContainerInView>
+                      ) : (
+                        <RevealContainerOnEnter>
+                          {content}
+                        </RevealContainerOnEnter>
+                      )}
+                    </>
+                  );
+                }}
+              </Media>
             </div>
             <div className={styles.text2}>
-              <p>
-                The application is dynamic hub of critical insights and
-                information built with cutting-edge web technology. Using
-                interactive visualizations, world class design, and information
-                hierarchy, the application cuts through the noise and provides
-                stakeholders with a single source of truth about the aircraft
-                they need insights on.
-              </p>
+              <RevealContainerInView>
+                <p>
+                  The application is dynamic hub of critical insights and
+                  information built with cutting-edge web technology. Using
+                  interactive visualizations, world class design, and
+                  information hierarchy, the application cuts through the noise
+                  and provides stakeholders with a single source of truth about
+                  the aircraft they need insights on.
+                </p>
+              </RevealContainerInView>
             </div>
             <div className={styles.image3}>
-              <ResponsiveImage
-                height={1160}
-                width={1450}
-                src='/images/f35/f35-mobile-phones.jpg'
-                altText='screenshot of caffeinator homepage'
-                bgColor={imagePlaceholderBackgroundColor}
-              />
+              <RevealContainerInView>
+                <ResponsiveImage
+                  height={1160}
+                  width={1450}
+                  src='/images/f35/f35-mobile-phones.jpg'
+                  altText='screenshot of caffeinator homepage'
+                  bgColor={imagePlaceholderBackgroundColor}
+                />
+              </RevealContainerInView>
             </div>
           </div>
         </SplitContentRight>
