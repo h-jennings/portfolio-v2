4ff8e59f2fb5885a85a50596f25bca41e9c67377
h-jennings <jenningsdhunter@gmail.com>
2021-02-09 17:33:25 -0500

    adding canonical url to pages

--- src/data/seo-data.ts
+++ src/data/seo-data.ts
@@ -1,32 +1,38 @@
 import { decodeHtml } from '@/helpers/decode-html';
+import { Paths } from '@/models/paths';
 
 const emDash = decodeHtml('&mdash;');
+const absoluteUrl = 'https://hunterjennings.dev';
 
 const pagesData: Record<
   string,
-  { title: string; description: string; imageUrl: string }
+  { title: string; description: string; imageUrl: string; canonical: string }
 > = {
   about: {
     title: `Portfolio ${emDash} About`,
     // eslint-disable-next-line quotes
     description: "I'm Hunter. I've been wrangling pixels and shapes since 2017",
     imageUrl: '/images/social/social-screen_about.png',
+    canonical: `${absoluteUrl}${Paths.about}`,
   },
   caffeinator: {
     title: `Portfolio ${emDash} Caffeinator`,
     description: 'An app for building the perfect cup of coffee.',
     imageUrl: '/images/social/social-screen_caffeinator.png',
+    canonical: `${absoluteUrl}${Paths.caffeinator}`,
   },
   ['portfolio-v1']: {
     title: `Portfolio ${emDash} V1`,
     description: 'My first portfolio, created in 2019.',
     imageUrl: '/images/social/social-screen_pv1.png',
+    canonical: `${absoluteUrl}${Paths.portfolioV1}`,
   },
   dod: {
     title: `Portfolio ${emDash} DoD`,
     description:
       'Web application for the next generation of defense technology',
     imageUrl: '/images/social/social-screen_dod.png',
+    canonical: `${absoluteUrl}${Paths.dod}`,
   },
 };
 
@@ -35,6 +41,7 @@ export const pagesMetaData = Object.fromEntries(
     const value = {
       title: val.title,
       description: val.description,
+      canonical: val.canonical,
       openGraph: {
         title: val.title,
         description: val.description,
